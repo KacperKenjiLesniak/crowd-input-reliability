d3c3c9e1affbc44dab67b15698320e6a41bdeac3
KacperKenjiLesniak <kenjikacper@gmail.com>
2021-02-13 15:55:51 +0100

    Removed MonoBehaviour implementation from the class

--- Packages/CrowdInputReliability/Runtime/CrowdInputReliability.cs
+++ Packages/CrowdInputReliability/Runtime/CrowdInputReliability.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class CrowdInputReliability : MonoBehaviour
+public class CrowdInputReliability
 {
     public List<float> playerReliabilities = new List<float>();
     public int numberOfPlayers { get; }
